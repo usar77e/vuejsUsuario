4ad9f44d1b0f0c0b903b403d2705059fa886324b
ERi <erick_dr_17@hotmail.com>
2022-11-17 11:44:36 -0300

    modificacion de botones
    solo acepta ciertos botones

--- api-cic-usuario/src/App.vue
+++ api-cic-usuario/src/App.vue
@@ -1,37 +1,17 @@
 <script setup>
   const name = 'Vue dinamico';
-  const arrayFrutas = [
-    {
-        name: "Manzana",
-        price: "$1.00",
-        description: "Una manzana",
-        stock: 0,
-    },
-    {
-        name: "Pera",
-        price: "$2.00",
-        description: "Una pera",
-        stock: 10,
-    },
-    {
-        name: "Naranja",
-        price: "$3.00",
-        description: "Una naranja",
-        stock: 20,
-    },
-];
+  //metodo
+  const handlelclick = (message) => {
+    console.log(message);
+  }
 </script>
 
 <template>
   <h1>Holas {{ name.toUpperCase() }} </h1>
   <br/>
-  <ul>
-    <template v-for="(value, index) in arrayFrutas" :key="index">
-      <li v-if="value.stock > 0">
-        {{ value.name }}: {{ value.price }}
-      </li>
-    </template>
-  </ul>
+  <button v-on:click.left="handlelclick('texto left')">Activame left</button>
+  <button @click.middle="handlelclick('texto middle')">Activame middle</button>
+  <button @click.right.prevent="handlelclick('texto right')">Activame right</button>
 </template>
 
 <style>
