9f562745ab782fc286c01885a9aa698591d96657
ERi <erick_dr_17@hotmail.com>
2022-11-17 16:06:14 -0300

    componentes (2)
    paso de informacion entre app.vue y componente

--- api-cic-usuario/src/App.vue
+++ api-cic-usuario/src/App.vue
@@ -1,9 +1,27 @@
 <script setup >
-  import  ButtonCounter  from './components/ButtonCounter.vue'
+  import { ref } from 'vue';
+  import  ButtonCounter  from './components/ButtonCounter.vue';
+  import BlogPost from './components/BlogPost.vue';
 
+
+  const posts = ref([
+    {title: 'Post 1', id: 1, body: 'descripcion larga1', colorText: 'primary'},
+    {title: 'Post 2', id: 2, body: 'descripcion 2', colorText: 'secondary'},
+    {title: 'Post 3', id: 3, body: 'descripcion 3', colorText: 'success'},
+    {title: 'Post 4', id: 4, body: 'descripcion 4', colorText: 'primary'},
+  ])
 </script>
 
 <template>
-  <h1>APP</h1>
-  <ButtonCounter/>
+  <div class="container">
+    <h1>APP</h1>
+    <ButtonCounter/>
+    <BlogPost v-for="post in posts"
+      :title="post.title"
+      :id="post.id"
+      :body = "post.body"
+      :colorText="post.colorText"
+    />
+  </div>
+  
 </template>
---
+++ api-cic-usuario/src/components/BlogPost.vue
@@ -0,0 +1,18 @@
+<script setup>
+    defineProps([
+        'title',
+        'body',
+        'id',
+        'colorText',
+    ])
+</script>
+<template>
+    <div class="card">
+        <div class="card-body"
+        :class="`text-${colorText}`">
+            <h5 class="card-title">{{ id }} - {{ title }}</h5>
+            <p>{{ body}}</p>
+        </div>
+    </div>
+
+</template>
