cd7991c20b7ccdeffe333b0e04d0a3b233fffab1
ERi <erick_dr_17@hotmail.com>
2022-11-17 10:19:16 -0300

    clases 18 a 21
    pruebas con v-if y v-show

--- api-cic-usuario/src/App.vue
+++ api-cic-usuario/src/App.vue
@@ -1,3 +1,33 @@
+<script setup>
+  const name = 'Vue dinamico';
+  const styleColor = "color: blue";
+  const arrayColores = ["blue", "red", "peru"];
+  const activo = true;
+</script>
+
 <template>
-  <h1>Hola mundo </h1>
+  <h1>Hola {{ name.toUpperCase() }} </h1>
+  <br/>
+  <h2 :style="styleColor">Soy azul</h2>
+  <br/>
+  <h2 :style="`color: ${arrayColores[2]}`" > {{ arrayColores }}</h2>
+  <br/>
+  <h2>
+    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
+  </h2>
+  <br/>
+  <p v-if="activo">Estoy activo</p>
+  <p v-else="activo">Estoy inactivo</p>
+  <h1>variacion de if</h1>
+  <p v-if="activo === true">Estoy activo</p>
+  <p v-else-if="activo === false">Estoy inactivo</p>
+  <p v-else>Estoy indeciso</p>
+  <h1>v-show</h1>
+  <h2 v-show="activo">Estoy activo v-show</h2>
 </template>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
